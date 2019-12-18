cc77a3e96a53da0cbfcac6718ad2396664f2fdd3
Ali KOCA <ali.koskocaman@gmail.com>
2019-12-18 12:23:52 +0300

    Lisans

---
+++ git.sh
@@ -0,0 +1,12 @@
+git config --global user.email "ali.koskocaman@gmail.com"
+git config --global user.name "Ali KOCA"
+
+
+git init
+cp README.md.misal README.md
+git add .
+git commit -m "Oluşturma"
+git remote add origin https://github.com/AliKOCA/Arduino-enc28j60-kutubhaneler.git
+git push -u origin master
+
+ali.koskocaman@gmail.com
